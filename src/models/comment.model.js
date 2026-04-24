f553d290f9863f27376be7bfb1ba4be45883d031
archisman <archidas2006@gmail.com>
2026-04-24 23:55:39 +0530

    add validate hook to comment schema

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
+import { ApiError } from '../utils/ApiError.js';
 const commentSchema=new mongoose.Schema(
     {
         content:{
@@ -18,11 +19,19 @@ const commentSchema=new mongoose.Schema(
             ref:'User',required:true        
         },
         isEdited:{
-            type:Boolean, 
+            type:Boolean, required:true
         }
     },{
         timestamps:true
     }
 )
+commentSchema.pre("validate", function (next) {
+    const hasVideo = this.video;
+    const hasTweet = this.tweet;
+    if (hasVideo === hasTweet) {
+        return next(new ApiError(400,"Comment must belong to either video or tweet, not both."));
+    }
+    next();
+});
 commentSchema.plugin(mongooseAggregatePaginate)
 export const Comment=mongoose.model('Comment',commentSchema);
