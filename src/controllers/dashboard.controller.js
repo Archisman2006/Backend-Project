63b372f480d189c8533e19b0613caaaafc1e483b
archisman <archidas2006@gmail.com>
2026-05-28 17:51:12 +0530

    add getChannelStats controller to retrieve channel statistics

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -60,3 +60,79 @@ const getChannelVideos=asynchandler(async (req,res)=>{
         new ApiResponse(200,videos,"All videos are fetched")
     )
 })
+const getChannelStats=asynchandler(async (req,res)=>{
+    const {channelId}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(channelId))
+        throw new ApiError(400,"invalid channel id")
+    // get channel total subscribers, total views, total likes, total videos posted
+    const channel=await User.findById(channelId);
+    if(!channel) throw new ApiError(404,"Channel not found")
+    const pipeline=[
+        {
+            $match:{
+                _id:mongoose.Types.ObjectId(channelId)
+            }
+        },{
+            $lookup:{
+                from:'subscriptions',
+                localField:'_id',
+                foreignField:'channel',
+                as:'subscribers',
+            }
+        },{
+            $lookup:{
+                from:'videos',
+                localField:'_id',
+                foreignField:'owner',
+                as:'videos',
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:'likes',
+                            localField:'_id',
+                            foreignField:'video',
+                            as:'likes'
+                        }
+                    },{
+                        $addFields:{
+                            likes:{
+                                $size:'$likes'
+                            }
+                        }
+                    }
+                ]
+            }
+        },{
+            $addFields:{
+                totalSubscribers:{
+                    $size:'$subscribers'
+                },
+                totalViews:{
+                    $sum:'$videos.views'
+                },
+                totalVideos:{
+                    $size:'$videos'
+                },
+                totalLikes:{
+                    $sum:'$videos.likes'
+                }
+            }
+        },{
+            $project:{
+                userName:1,
+                fullName:1,
+                avatar:1,
+                coverImage:1,
+                totalSubscribers:1,
+                totalLikes:1,
+                totalViews:1,
+                totalVideos:1
+            }
+        }
+    ]
+    const stats=await User.aggregate(pipeline);
+    return res.status(200).json(
+        new ApiResponse(200,stats,"channel statistics successfully retrieved")
+    )
+})
+export {getChannelVideos,getChannelStats}
