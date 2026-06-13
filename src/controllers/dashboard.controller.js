4c40fc565b1a24d043a9539a141f1325b0881bce
archisman <archidas2006@gmail.com>
2026-06-13 09:22:39 +0530

    add controllers to get channel tweets and playlists

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -3,21 +3,21 @@ import { asynchandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
 import { Video } from "../models/video.models.js";
+import { Tweet } from "../models/tweet.model.js";
+import { Playlist } from "../models/playlist.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const getChannelVideos=asynchandler(async (req,res)=>{
-    const {channelId}=req.params;
+    const {username}=req.params;
     const {page,limit}=req.query;
     const parsedPage=Math.max(parseInt(page,10)|| 1,1)
     const parsedLimit=Math.min(Math.max(parseInt(limit,10) || 10,1),50)
-    if(!mongoose.Types.ObjectId.isValid(channelId))
-        throw new ApiError(400,"channel id is invalid")
-    const channel=await User.findById(channelId);
+    const channel=await User.findOne({username});
     if(!channel) throw new ApiError(404,"Channel not found")
     const pipeline=[
         {
             $match:{
-                owner:new mongoose.Types.ObjectId(channelId)
+                owner:new mongoose.Types.ObjectId(channel._id)
             }
         },{
             $lookup:{
@@ -50,7 +50,7 @@ const getChannelVideos=asynchandler(async (req,res)=>{
             }
         }
     ]
-    const isOwner=(req.user?._id.toString()===channelId.toString());
+    const isOwner=(req.user?._id.toString()===channel._id.toString());
     if(!isOwner) pipeline.push({$match:{isPublished:true}})
     const aggregate=Video.aggregate(pipeline);
     const videos=await Video.aggregatePaginate(aggregate,{
@@ -58,6 +58,109 @@ const getChannelVideos=asynchandler(async (req,res)=>{
     })
     return res.status(200).json(
         new ApiResponse(200,videos,"All videos are fetched")
+    )
+})
+const getChannelTweets=asynchandler(async (req,res)=>{
+    const {username}=req.params;
+    const {page,limit}=req.query;
+    const parsedPage=Math.max(parseInt(page,10)|| 1,1)
+    const parsedLimit=Math.min(Math.max(parseInt(limit,10) || 10,1),50)
+    const channel=await User.findOne({username});
+    if(!channel) throw new ApiError(404,"Channel not found")
+    const pipeline=[
+        {
+            $match:{
+                owner:new mongoose.Types.ObjectId(channel._id)
+            }
+        },{
+            $lookup:{
+                from:'users',
+                localField:'owner',
+                foreignField:'_id',
+                as:'owner',
+                pipeline:[
+                    {
+                        $project:{
+                            username:1,
+                            fullName:1,
+                            avatar:1
+                        }
+                    }
+                ]
+            }
+        },{
+            $addFields:{
+                owner:{
+                    $first:'$owner'
+                }
+            }
+        },{
+            $project:{
+                _id:1,
+                content:1,
+                image:1,
+                owner:1
+            }
+        }
+    ]
+    const aggregate=Tweet.aggregate(pipeline);
+    const tweets=await Tweet.aggregatePaginate(aggregate,{
+        page:parsedPage,limit:parsedLimit
+    });
+    return res.status(200).json(
+        new ApiResponse(200,tweets,"channel tweets retreived successfully")
+    )
+})
+const getChannelPlaylists=asynchandler(async (req,res)=>{
+    const {username}=req.params;
+    const {page,limit}=req.query;
+    const parsedPage=Math.max(parseInt(page,10)|| 1,1)
+    const parsedLimit=Math.min(Math.max(parseInt(limit,10) || 10,1),50)
+    const channel=await User.findOne({username});
+    if(!channel) throw new ApiError(404,"Channel not found")
+    const pipeline=[
+        {
+            $match:{
+                owner:new mongoose.Types.ObjectId(channel._id)
+            }
+        },{
+            $lookup:{
+                from:'users',
+                localField:'owner',
+                foreignField:'_id',
+                as:'owner',
+                pipeline:[
+                    {
+                        $project:{
+                            username:1,
+                            fullName:1,
+                            avatar:1
+                        }
+                    }
+                ]
+            }
+        },{
+            $addFields:{
+                owner:{
+                    $first:'$owner'
+                }
+            }
+        },{
+            $project:{
+                _id:1,
+                name:1,
+                description:1,
+                videos:1,
+                owner:1
+            }
+        }
+    ]
+    const aggregate=Playlist.aggregate(pipeline);
+    const playlists=await Playlist.aggregatePaginate(aggregate,{
+        page:parsedPage,limit:parsedLimit
+    });
+    return res.status(200).json(
+        new ApiResponse(200,playlists,"channel playlists retreived successfully")
     )
 })
 const getChannelStats=asynchandler(async (req,res)=>{
@@ -135,4 +238,4 @@ const getChannelStats=asynchandler(async (req,res)=>{
         new ApiResponse(200,stats,"channel statistics successfully retrieved")
     )
 })
-export {getChannelVideos,getChannelStats}
+export {getChannelVideos,getChannelTweets,getChannelPlaylists,getChannelStats}
--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,8 +1,11 @@
-import {getChannelVideos,getChannelStats} from '../controllers/dashboard.controller.js'
+import {getChannelVideos,getChannelStats,getChannelTweets,getChannelPlaylists}
+from '../controllers/dashboard.controller.js'
 import { Router } from 'express'
 import { VerifyJWT } from '../middlewares/auth.middleware.js'
 const router=Router();
 router.use(VerifyJWT);
 router.route('/:channelId').get(getChannelStats)
-router.route('/videos/:channelId').get(getChannelVideos)
+router.route('/videos/:username').get(getChannelVideos)
+router.route('/tweets/:username').get(getChannelTweets)
+router.route('/playlists/:username').get(getChannelPlaylists)
 export default router
