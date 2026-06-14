0a2bf481a9038da8d32f23baac5e80c3bbf48f59
archisman <archidas2006@gmail.com>
2026-06-14 19:36:59 +0530

    bug fix in optionalVerifyJWT middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -28,7 +28,7 @@ export const OptionalVerifyJWT=asynchandler(async (req,res,next)=>{
         const token=
     req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
     if(!token){
-        req.user=null; next();
+        req.user=null; return next();
     }
     const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
     const user= await User.findById(decodedToken).select("-password -RefreshToken")
