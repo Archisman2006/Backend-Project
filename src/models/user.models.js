b06758bb632bab39716668e07f66bdab26ece622
archisman <archidas2006@gmail.com>
2026-07-07 10:04:16 +0530

    refactor user model to make password not required and add googleId for google sign in.

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -14,17 +14,20 @@ const userSchema=new mongoose.Schema(
             type:String, required:true,trim:true
         },
         avatar:{
-            type:String, required:true,unique:true,lowercase:true,trim:true
+            type:String, required:true,trim:true
         },
         coverImage:{
             type:String
+        },
+        googleId:{
+            type:String,unique:true,sparse:true
         },
         watchHistory:[{
             type:mongoose.Schema.Types.ObjectId,
             ref:'Video'
         }],
         password:{
-            type:String, required:[true,'Password is required']
+            type:String
         },
         refreshToken:{
             type:String
@@ -44,10 +47,11 @@ const userSchema=new mongoose.Schema(
     {timestamps:true}
 )
 userSchema.pre("save",async function (next){
-    if(!this.isModified("password")) { return;}
+    if(!this.isModified("password") || !this.password) { return;}
     this.password=await bcrypt.hash(this.password,10);
 })
 userSchema.methods.isPasswordCorrect=async function(password){
+    if(!password) return false;
     return await bcrypt.compare(password,this.password);
 } 
 userSchema.methods.generateAccessToken=function(){
